277ed4dd768c0d762f234a742760b43b095bc4f8
zhuangwang93 <wangzhuangowl@gmail.com>
2026-04-02 22:56:00 +0000

    add POST example for peer-to-peer transfer across instances

--- examples/openai_request.sh
+++ examples/openai_request.sh
@@ -5,7 +5,6 @@ curl -X POST http://localhost:8000/completions \
 curl -X POST http://localhost:8000/sleep
 curl -X POST http://localhost:8000/wake_up
 
-
 curl -X POST http://localhost:8001/completions \
   -H "Content-Type: application/json" \
   -d '{"prompt": "The capital of France is", "max_tokens": 32}'
@@ -18,10 +17,12 @@ curl -X POST http://localhost:8001/sleep
 curl -X POST http://localhost:8001/wake_up
 
 
-curl -X POST http://localhost:8002/wake_up \
-  -H "Content-Type: application/json" \
-  -d '{"peer_url": "http://localhost:8000"}'
-
+# peer-to-peer weight transfer within instances
 curl -X POST http://localhost:8001/wake_up \
   -H "Content-Type: application/json" \
   -d '{"peer_url": "http://localhost:8002"}'
+
+# peer-to-peer weight transfer across instances
+curl -X POST http://localhost:8001/wake_up \
+  -H "Content-Type: application/json" \
+  -d '{"peer_url": "http://172.31.44.131:8000"}'
